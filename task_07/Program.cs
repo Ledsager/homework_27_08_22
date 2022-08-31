1dd950dd2d9bbac62f6d68e3a9f825a9697c8677
WIN-JJH83PJTM7J\mkadmin <r.yakupov81@yandex.ru>
2022-08-31 13:23:22 +0300

    added task_07

--- task_07/Program.cs
+++ task_07/Program.cs
@@ -24,15 +24,42 @@ void ElementCube(int cube)
     Console.WriteLine($"{Math.Pow(cube, 3)}");
 }
 // Метод проверки знака числа
-bool ElementEven(int num)
+bool ElementPositiv(int num)
 {
     bool result = false;
-    if (num % 2 == 0)
+    if (num >= 0)
         result = true;
     return result;
+}
+void SumPositivAndNegativElement(int[] mas)
+{
+    int sumpositiv = 0;
+    int yespositiv = 0, nopositiv = 0;
+
+    int sumnegativ = 0;
+    foreach (int element in mas)
+        if (ElementPositiv(element) == true)
+        {
+            yespositiv++;
+            sumpositiv = sumpositiv + element;
+        }
+        else
+        {
+            nopositiv++;
+            sumnegativ = sumnegativ + element;
+        }
+    if(yespositiv > 0)
+        Console.WriteLine($"Сумма положительных элементов массива = {sumpositiv}");
+    else
+        Console.WriteLine($"положительных элементов в массиве нет");
+    if(nopositiv > 0)
+        Console.WriteLine($"Сумма отрицательных элементов массива = {sumnegativ}");
+    else
+        Console.WriteLine($"отрицательных элементов в массиве нет");
 }
 Console.Write("Введите длинну массива:");
 int LengthArray = int.Parse(Console.ReadLine());
 int[] array = new int[LengthArray];
-RandomArrayElement(array, 0, 10);// Генерация элементов масива в заданном диапазоне
-
+RandomArrayElement(array, -10, 10);// Генерация элементов масива в заданном диапазоне
+PrintArray(array);
+SumPositivAndNegativElement(array);
